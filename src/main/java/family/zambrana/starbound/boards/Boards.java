aea78abe9e6d3de4cfc943bc45f247ac14fbab25
Aduhkiss <angusbeefsteak14@gmail.com>
2025-07-07 13:19:56 -0700

    boards code

---
+++ src/main/java/family/zambrana/starbound/boards/Boards.java
@@ -0,0 +1,9 @@
+package family.zambrana.starbound.boards;
+
+import family.zambrana.starbound.MiniPlugin;
+
+public class Boards extends MiniPlugin {
+    public Boards() {
+        super("Boards");
+    }
+}
